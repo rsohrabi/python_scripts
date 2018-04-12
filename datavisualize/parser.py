5238ecab799709638d9fd9acad97bc60c64859f8
rocky sohrabi <rockysohrabi@hotmail.com>
2018-04-12 13:21:31 -0500

    Parser for CSV sample data file processing.

--- datavisualize/parser.py
+++ datavisualize/parser.py
@@ -8,32 +8,36 @@ Convert JSON data into graphs.
 
 import csv
 
-def parser(raw_file, delimiter):
+
+def parser(csv_file, delimiter):
     """Parses a raw CSV file to a JSON-like object"""
 
     # Open CSV input file
-    with open(raw_file, 'rb') as f:
+    with open(csv_file) as f:
+        # Read the CSV data
+        csv_data = csv.reader(f, delimiter=delimiter)
 
-      # Read the CSV data
-      csv_data = csv.reader(f, delimiter=delimiter)
+        parsed_data = []
 
-      parsed_data = []
+        # Skip over the first line -- headers
+        fields = next(csv_data)
+       
+        # Iterate over each row of the csv file, zip together field -> value
+        for row in csv_data:
+            parsed_data.append(dict(zip(fields, row)))
 
-      # Skip over the first line -- headers
-      fields = csv_data.next()
-
-      # Iterate over each row of the csv file, zip together field -> value
-      for row in csv_data:
-          parsed_data.append(dict(zip(fields, row)))
-
-      return parsed_data
+    return parsed_data
 
 
 def main():
     # Call parser and give needed parameters.
     new_data = parser(MY_FILE, ",")
-    print new_data
+    print(new_data)
 
+
+# sample data to inject into application.
+
+MY_FILE = r'data/sample_sfpd_incident_all.csv'
 
 if __name__ == "__main__":
     main()
